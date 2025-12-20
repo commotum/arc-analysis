8b6ba286974529732eabb13cc5320a5c30c49eec
JP <jneilpeterson@gmail.com>
2025-12-19 18:08:18 -0800

    .

--- int-id_hex-id_solo.py
+++ int-id_hex-id_solo.py
@@ -198,7 +198,6 @@ T_OUT = np.array([
 
 
 # --- Code Golf Solution (Barnacles) ---
-
 def p(j,A=range):
  c,E=len(j),len(j[0])
  p=[(l,L,j[l][L])for l in A(c)for L in A(E)if j[l][L]]
@@ -244,7 +243,345 @@ def p(j,A=range):
  return j
 
 
- # --- Code Golf Solution (Barnacles) ---
-
-
+ # --- Code Golf Solution (Compressed) ---
 p=lambda g,h=0,l=[]:[[(l:=[max(c+[j*(i>0)for i,j in zip(l,l[1::2])])]+l)[:1]*len(c),c][c[1:-1]>c]for*c,in zip(*h or p(g,g))]
+
+
+# --- RE-ARC Generator ---
+from typing import (
+    Tuple,
+)
+
+from random import uniform, sample, choice
+
+Integer = int
+
+def interval(
+    start: Integer,
+    stop: Integer,
+    step: Integer
+) -> Tuple:
+    """ range """
+    return tuple(range(start, stop, step))
+
+def unifint(
+    diff_lb: float,
+    diff_ub: float,
+    bounds: Tuple[int, int]
+) -> int:
+    """
+    diff_lb: lower bound for difficulty, must be in range [0, diff_ub]
+    diff_ub: upper bound for difficulty, must be in range [diff_lb, 1]
+    bounds: interval [a, b] determining the integer values that can be sampled
+    """
+    a, b = bounds
+    d = uniform(diff_lb, diff_ub)
+    global rng
+    rng.append(d)
+    return min(max(a, round(a + (b - a) * d)), b)
+
+IntegerTuple = Tuple[Integer, Integer]
+Grid = Tuple[Tuple[Integer]]
+
+from typing import (
+    List,
+    Union,
+    Tuple,
+    Any,
+    Container,
+    Callable,
+    FrozenSet,
+    Iterable
+)
+
+Boolean = bool
+Integer = int
+IntegerTuple = Tuple[Integer, Integer]
+Numerical = Union[Integer, IntegerTuple]
+IntegerSet = FrozenSet[Integer]
+Grid = Tuple[Tuple[Integer]]
+Cell = Tuple[Integer, IntegerTuple]
+Object = FrozenSet[Cell]
+Objects = FrozenSet[Object]
+Indices = FrozenSet[IntegerTuple]
+IndicesSet = FrozenSet[Indices]
+Patch = Union[Object, Indices]
+Element = Union[Object, Grid]
+Piece = Union[Grid, Patch]
+TupleTuple = Tuple[Tuple]
+ContainerContainer = Container[Container]
+
+
+
+# constants
+
+
+ZERO = 0
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+SIX = 6
+SEVEN = 7
+EIGHT = 8
+NINE = 9
+TEN = 10
+F = False
+T = True
+
+NEG_ONE = -1
+
+ORIGIN = (0, 0)
+UNITY = (1, 1)
+DOWN = (1, 0)
+RIGHT = (0, 1)
+UP = (-1, 0)
+LEFT = (0, -1)
+
+NEG_TWO = -2
+NEG_UNITY = (-1, -1)
+UP_RIGHT = (-1, 1)
+DOWN_LEFT = (1, -1)
+
+ZERO_BY_TWO = (0, 2)
+TWO_BY_ZERO = (2, 0)
+TWO_BY_TWO = (2, 2)
+THREE_BY_THREE = (3, 3)
+
+def canvas(
+    value: Integer,
+    dimensions: IntegerTuple
+) -> Grid:
+    """ grid construction """
+    return tuple(tuple(value for j in range(dimensions[1])) for i in range(dimensions[0]))
+
+def toindices(
+    patch: Patch
+) -> Indices:
+    """ indices of object cells """
+    if len(patch) == 0:
+        return frozenset()
+    if isinstance(next(iter(patch))[1], tuple):
+        return frozenset(index for value, index in patch)
+    return patch
+
+def fill(
+    grid: Grid,
+    value: Integer,
+    patch: Patch
+) -> Grid:
+    """ fill value at indices """
+    h, w = len(grid), len(grid[0])
+    grid_filled = list(list(row) for row in grid)
+    for i, j in toindices(patch):
+        if 0 <= i < h and 0 <= j < w:
+            grid_filled[i][j] = value
+    return tuple(tuple(row) for row in grid_filled)
+
+def connect(
+    a: IntegerTuple,
+    b: IntegerTuple
+) -> Indices:
+    """ line between two points """
+    ai, aj = a
+    bi, bj = b
+    si = min(ai, bi)
+    ei = max(ai, bi) + 1
+    sj = min(aj, bj)
+    ej = max(aj, bj) + 1
+    if ai == bi:
+        return frozenset((ai, j) for j in range(sj, ej))
+    elif aj == bj:
+        return frozenset((i, aj) for i in range(si, ei))
+    elif bi - ai == bj - aj:
+        return frozenset((i, j) for i, j in zip(range(si, ei), range(sj, ej)))
+    elif bi - ai == aj - bj:
+        return frozenset((i, j) for i, j in zip(range(si, ei), range(ej - 1, sj - 1, -1)))
+    return frozenset()
+
+def rot180(
+    grid: Grid
+) -> Grid:
+    """ half rotation """
+    return tuple(tuple(row[::-1]) for row in grid[::-1])
+
+def rot270(
+    grid: Grid
+) -> Grid:
+    """ quarter anticlockwise rotation """
+    return tuple(tuple(row[::-1]) for row in zip(*grid[::-1]))[::-1]
+
+
+def generate_0a938d79(diff_lb: float, diff_ub: float) -> dict:
+    cols = interval(0, 10, 1)
+    h = unifint(diff_lb, diff_ub, (4, 29))
+    w = unifint(diff_lb, diff_ub, (h+1, 30))
+    bgc, cola, colb = sample(cols, 3)
+    gi = canvas(bgc, (h, w))
+    go = canvas(bgc, (h, w))
+    locja = unifint(diff_lb, diff_ub, (3, w - 2))
+    locjb = unifint(diff_lb, diff_ub, (1, locja - 2))
+    locia = choice((0, h-1))
+    locib = choice((0, h-1))
+    gi = fill(gi, cola, {(locia, locja)})
+    gi = fill(gi, colb, {(locib, locjb)})
+    ofs = -2 * (locja-locjb)
+    for aa in range(locja, -1, ofs):
+        go = fill(go, cola, connect((0, aa), (h-1, aa)))
+    for bb in range(locjb, -1, ofs):    
+        go = fill(go, colb, connect((0, bb), (h-1, bb)))
+    rotf = choice((rot180, rot270))
+    gi = rotf(gi)
+    go = rotf(go)
+    return {'input': gi, 'output': go}
+
+
+# --- RE-ARC Verifier ---
+def rightmost(
+    patch: Patch
+) -> Integer:
+    """ column index of rightmost occupied cell """
+    return max(j for i, j in toindices(patch))
+
+def leftmost(
+    patch: Patch
+) -> Integer:
+    """ column index of leftmost occupied cell """
+    return min(j for i, j in toindices(patch))
+
+def lowermost(
+    patch: Patch
+) -> Integer:
+    """ row index of lowermost occupied cell """
+    return max(i for i, j in toindices(patch))
+
+def uppermost(
+    patch: Patch
+) -> Integer:
+    """ row index of uppermost occupied cell """
+    return min(i for i, j in toindices(patch))
+
+
+def height(
+    piece: Piece
+) -> Integer:
+    """ height of grid or patch """
+    if len(piece) == 0:
+        return 0
+    if isinstance(piece, tuple):
+        return len(piece)
+    return lowermost(piece) - uppermost(piece) + 1
+
+def width(
+    piece: Piece
+) -> Integer:
+    """ width of grid or patch """
+    if len(piece) == 0:
+        return 0
+    if isinstance(piece, tuple):
+        return len(piece[0])
+    return rightmost(piece) - leftmost(piece) + 1
+
+
+def portrait(
+    piece: Piece
+) -> Boolean:
+    """ whether height is greater than width """
+    return height(piece) > width(piece)
+
+def branch(
+    condition: Boolean,
+    if_value: Any,
+    else_value: Any
+) -> Any:
+    """ if else branching """
+    return if_value if condition else else_value
+
+def dmirror(
+    piece: Piece
+) -> Piece:
+    """ mirroring along diagonal """
+    if isinstance(piece, tuple):
+        return tuple(zip(*piece))
+    a, b = ulcorner(piece)
+    if isinstance(next(iter(piece))[1], tuple):
+        return frozenset((v, (j - b + a, i - a + b)) for v, (i, j) in piece)
+    return frozenset((j - b + a, i - a + b) for i, j in piece)
+
+def ulcorner(
+    patch: Patch
+) -> IntegerTuple:
+    """ index of upper left corner """
+    return tuple(map(min, zip(*toindices(patch))))
+
+def identity(
+    x: Any
+) -> Any:
+    """ identity function """
+    return x
+
+Objects = FrozenSet[Object]
+
+def objects(
+    grid: Grid,
+    univalued: Boolean,
+    diagonal: Boolean,
+    without_bg: Boolean
+) -> Objects:
+    """ objects occurring on the grid """
+    bg = mostcolor(grid) if without_bg else None
+    objs = set()
+    occupied = set()
+    h, w = len(grid), len(grid[0])
+    unvisited = asindices(grid)
+    diagfun = neighbors if diagonal else dneighbors
+    for loc in unvisited:
+        if loc in occupied:
+            continue
+        val = grid[loc[0]][loc[1]]
+        if val == bg:
+            continue
+        obj = {(val, loc)}
+        cands = {loc}
+        while len(cands) > 0:
+            neighborhood = set()
+            for cand in cands:
+                v = grid[cand[0]][cand[1]]
+                if (val == v) if univalued else (v != bg):
+                    obj.add((v, cand))
+                    occupied.add(cand)
+                    neighborhood |= {
+                        (i, j) for i, j in diagfun(cand) if 0 <= i < h and 0 <= j < w
+                    }
+            cands = neighborhood - occupied
+        objs.add(frozenset(obj))
+    return frozenset(objs)
+
+
+
+def verify_0a938d79(I: Grid) -> Grid:
+    x0 = portrait(I)
+    x1 = branch(x0, dmirror, identity)
+    x2 = x1(I)
+    x3 = objects(x2, T, F, T)
+    x4 = argmin(x3, leftmost)
+    x5 = argmax(x3, leftmost)
+    x6 = color(x4)
+    x7 = color(x5)
+    x8 = leftmost(x4)
+    x9 = leftmost(x5)
+    x10 = subtract(x9, x8)
+    x11 = double(x10)
+    x12 = multiply(THREE, TEN)
+    x13 = interval(x8, x12, x11)
+    x14 = interval(x9, x12, x11)
+    x15 = compose(vfrontier, tojvec)
+    x16 = mapply(x15, x13)
+    x17 = mapply(x15, x14)
+    x18 = recolor(x6, x16)
+    x19 = recolor(x7, x17)
+    x20 = combine(x18, x19)
+    x21 = paint(x2, x20)
+    x22 = x1(x21)
+    return x22
